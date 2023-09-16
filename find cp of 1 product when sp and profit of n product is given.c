7ed5325bdeb68201fa2b2dd0ede838957e954ef4
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-09-16 23:54:26 +0500

    new update

--- find cp of 1 product when sp and profit of n product is given.c
+++ find cp of 1 product when sp and profit of n product is given.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    int pf;
+    int cp;
+    int sp;
+    int a;
+    int n;
+    
+    printf("Enter number of products:  ");
+    scanf("%d", &n);
+    printf("Enter selling price of products:  ");
+    scanf("%d", &sp);
+    printf("Enter profit of products:  ");
+    scanf("%d", &pf);
+    cp = sp-pf;
+    a= cp/n;
+    printf("Cost price of one item:  %d",a);
+    
+
+    return 0;
+}
